1083d54ac8cbb9bb7506596ecb7516860e6e92c6
zhs007 <sssxueren@gmail.com>
2018-10-08 21:38:54 +0800

    upd

---
+++ samples/importcsv/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"encoding/json"
+	"io"
+	"os"
+
+	"github.com/zhs007/ankadb/client"
+)
+
+func importCSV(ctx context.Context, filename string, c ankadbclient.AnkaClient) error {
+	strQuery := `mutation InsertCandles($code:String!, $name:String!, $candle:CandleInput){
+		insertCandles(code:$code,name:$name,candle:$candle){
+			code,
+			name}
+		}`
+	fr, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	csvr := csv.NewReader(fr)
+
+	for {
+		record, err := csvr.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		mapval := make(map[string]interface{})
+		mapval["code"] = record[0]
+
+		buf, err1 := json.Marshal(mapval)
+		if err1 != nil {
+			return err1
+		}
+
+		c.Query(ctx, strQuery, string(buf))
+		// fmt.Println(record)
+	}
+
+	return nil
+}
+
+func main() {
+	c := ankadbclient.NewClient()
+	c.Start("")
+}
