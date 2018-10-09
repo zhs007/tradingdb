f7b3336daa96d50428e1e4c5e6b84d82042c177b
zhs007 <sssxueren@gmail.com>
2018-10-09 22:57:11 +0800

    upd

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
 docker container stop tradingdb
 docker container rm tradingdb
-docker run -d -p 7888:7888 -p 7889:7889 --name tradingdb -v $PWD/dat:/home/tradingdb/dat -v $PWD/cfg:/home/tradingdb/cfg tradingdb
+docker run -d -p 7788:7788 -p 7789:7789 --name tradingdb -v $PWD/dat:/home/tradingdb/dat -v $PWD/cfg:/home/tradingdb/cfg tradingdb
