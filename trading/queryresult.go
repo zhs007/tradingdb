39f04977f078f4c83c684ec71601a70cac2d53b1
zhs007 <sssxueren@gmail.com>
2018-10-16 22:21:12 +0800

    upd

--- trading/queryresult.go
+++ trading/queryresult.go
@@ -19,3 +19,33 @@ type ResultCandleChunks struct {
 		} `json:"candleChunks"`
 	} `json:"data"`
 }
+
+// ResultTradingData -
+type ResultTradingData struct {
+	Data struct {
+		TradingData struct {
+			KeyID string `json:"keyID"`
+
+			Orders []struct {
+				OrderID   string `json:"orderID"`
+				OrderType string `json:"orderType"`
+				OrderSide string `json:"orderSide"`
+				Price     int64  `json:"price"`
+				Volume    int64  `json:"volume"`
+				StartTime int64  `json:"startTime"`
+
+				AvgPrice   int64 `json:"avgPrice"`
+				DoneVolume int64 `json:"doneVolume"`
+				DoneTime   int64 `json:"doneTime"`
+			} `json:"orders"`
+
+			Trades []struct {
+				TradeID string `json:"tradeID"`
+				OrderID string `json:"orderID"`
+				CurTime int64  `json:"curTime"`
+				Price   int64  `json:"price"`
+				Volume  int64  `json:"volume"`
+			} `json:"trades"`
+		} `json:"tradingData"`
+	} `json:"data"`
+}
